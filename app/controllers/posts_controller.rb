7fac2f603880320b7d61e355a4d46f75ad8ab878
Andy Uppendahl <a.uppendahl@gmail.com>
2015-03-20 16:23:38 -0700

    Add new posts form with functionality for adding new post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,23 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Blog post successfully saved!"
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
