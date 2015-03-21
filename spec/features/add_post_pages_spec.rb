606f20f332a0c648204290190c8ecab163c5d23b
Andy Uppendahl <a.uppendahl@gmail.com>
2015-03-20 20:00:48 -0700

    Add successfully passed integration test for the 'add post pages' process'.

---
+++ spec/features/add_post_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "the add a post process" do
+  it "takes you to a new post page" do
+    visit '/'
+    click_on 'Add New Post'
+    expect(page).to have_content 'New Post'
+  end
+
+  it "adds a new post" do
+    visit '/'
+    click_on 'Add New Post'
+    fill_in 'Title', :with => 'My first week at camp'
+    fill_in 'Body', :with => 'Best week ever!'
+    click_on 'Create Post'
+    expect(page).to have_content 'Posts'
+  end
+
+  it "gives error when no post title is entered" do
+    visit '/'
+    click_on 'Add New Post'
+    click_on 'Create Post'
+    expect(page).to have_content 'errors'
+  end
+end
