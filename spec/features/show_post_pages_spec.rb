103d5ef0bae58df756132f3b7e27aa111b08f096
Andy Uppendahl <a.uppendahl@gmail.com>
2015-03-20 20:06:47 -0700

    Add successfully passed integration test for the 'show post detail page' process'.

---
+++ spec/features/show_post_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'the show post detail process' do
+  it 'shows the detail for a single post when clicked on ' \
+    'from home page' do
+    post = Post.create(
+      title: 'Two weeks to go!',
+      body: 'Almost there!'
+    )
+    visit '/'
+    click_on 'Two weeks to go!'
+    expect(page).to have_content 'weeks'
+  end
+end
