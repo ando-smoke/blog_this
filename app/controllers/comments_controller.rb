6073c7c00eefe0a380d26113f9e71f6f4bc003d3
Andy Uppendahl <a.uppendahl@gmail.com>
2015-03-20 19:52:47 -0700

    Add functionality for deleting an individual comment tied to a post.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -31,6 +31,14 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    flash[:notice] = "Comment successfully deleted!"
+    redirect_to post_path(@post)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:body)
