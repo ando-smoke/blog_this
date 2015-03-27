450446fbe157ec704bb75d2fc5a4258d5d09d1f2
Andy Uppendahl <a.uppendahl@gmail.com>
2015-03-27 16:57:26 -0700

    Add sign_in of user requirement to spec.

--- spec/features/delete_comment_pages_spec.rb
+++ spec/features/delete_comment_pages_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'the delete a comment process' do
   it 'edits the body of a preexisting comment' do
     comment = FactoryGirl.create(:comment)
+    sign_in(comment.post.user)
     visit post_path(comment.post)
     click_on 'Delete'
     expect(page).to have_content 'deleted!'
