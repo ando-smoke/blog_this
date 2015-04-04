d510889401a9c4495143255195863a6e020bcb0c
Andy Uppendahl <a.uppendahl@gmail.com>
2015-04-03 17:45:33 -0700

    Add phone_number field to user model and Twilio sms send for all new comments.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
     if resource.is_a? User
       posts_path
@@ -10,4 +12,10 @@ class ApplicationController < ActionController::Base
       root_path
     end
   end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :phone_number
+    end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,28 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
   validates :body, presence: true
+
+  after_create :send_message
+
+  private
+
+    def send_message
+      begin
+        response = RestClient::Request.new(
+          :method => :post,
+          :url => "https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json",
+          :user => ENV['TWILIO_ACCOUNT_SID'],
+          :password => ENV['TWILIO_AUTH_TOKEN'],
+          :payload => {
+            :Body => "New comment added to question: " +
+              self.post.title,
+            :To => self.post.user.phone_number,
+            :From => "+15005550006" }
+        ).execute
+      rescue RestClient::BadRequest => error
+        message = JSON.parse(error.response)['message']
+        errors.add(:base, message)
+        false
+      end
+    end
 end
